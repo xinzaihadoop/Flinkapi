81dbf3d15f98701f13840e59d467b76e90e6b556
MovieBook_xinll <17674033556@163.com>
2021-05-20 10:00:16 +0800

    init project

--- src/main/java/flink/streamingapi/source/SourceTest03_Kafka.java
+++ src/main/java/flink/streamingapi/source/SourceTest03_Kafka.java
@@ -1,5 +1,12 @@
 package flink.streamingapi.source;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
 /**
  * @ClassName: SourceTest03_Kafka
  * @Description: TODO
@@ -8,4 +15,23 @@ package flink.streamingapi.source;
  * @Version: v1.0
  */
 public class SourceTest03_Kafka {
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "10.65.55.11:9092");
+        properties.setProperty("group.id", "test-topic-[0-3]");
+
+
+        FlinkKafkaConsumer flinkKafkaConsumer = new FlinkKafkaConsumer<>("MovieBook_xll_test",new SimpleStringSchema(),properties);
+
+        DataStreamSource dataStreamSource = env.addSource(flinkKafkaConsumer);
+
+        dataStreamSource.print("kafka value is:");
+
+        env.execute();
+
+
+    }
 }
